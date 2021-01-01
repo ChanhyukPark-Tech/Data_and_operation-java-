8905da09858214114dd3f972f2619073418a5c04
ChanhyukPark-Tech <pgg6713@gmail.com>
2021-01-02 02:28:08 +0900

    Number.java
    not go into file make file >>> class instead of file
    Math.ceil Math.floor Math.PI

---
+++ Data_and_operation/Number.java
@@ -0,0 +1,17 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// Operator 
+		System.out.println(6 + 2); // 8
+		System.out.println(6 - 2);
+		System.out.println(6 * 2);
+		System.out.println(6 / 2);
+		
+		System.out.println(Math.PI); // 3.14--------------
+		System.out.println(Math.floor(Math.PI)); // 3
+		System.out.println(Math.ceil(Math.PI)); // 4
+		
+	}
+
+}

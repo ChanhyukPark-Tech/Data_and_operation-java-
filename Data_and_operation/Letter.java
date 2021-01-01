e3f6cf7cb01d3185220f5a69f4cdeb2fd584578e
ChanhyukPark-Tech <pgg6713@gmail.com>
2021-01-02 02:59:09 +0900

    efficiency of variable
    for future me
    
    2021-01-02

---
+++ Data_and_operation/Letter.java
@@ -0,0 +1,13 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "leezche";
+		System.out.println("Hello, "+name+" ... "+name+" ..... egoing ....bye ");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);        // 와 !!!!!!!!!!!!!!변수의 효용 10이라고쓰지말고 변수한번 지정해라 VAT !! 미래의 내가 코드의의미 
+		
+	}
+
+}

201f535e2cc93692b7ec0dbe50a49f1e8b1ee489
ChanhyukPark-Tech <pgg6713@gmail.com>
2021-01-02 02:38:07 +0900

    string 문자열에 대해 살펴봤다
    string = 문자열 character 들이 모여서
    character = 문자 (한글자한글자)
    띄어쓰기 \n 사용 escape \ 사용
    
    2021-01-02

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,18 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); // String 캐릭터들이 모여있는 문자열을 나타낸다 ! 
+    //  System.out.println('Hello World'); // Character 문자를 나타낸다 한글자를 나타내는 데이터타입
+		System.out.println('H');
+		System.out.println("Hello "
+				+ "World");
+		// new line
+		System.out.println("Hello \nWorld");  // 띄어쓰기 
+		
+		// escape 
+		System.out.println("Hello \"World\"");// Hello "World" 만약 진짜 " 를 표현하고 싶을때 escape 사용
+				
+	}
+
+}
